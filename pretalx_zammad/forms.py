501ca5548a8a7c5804fb63a0332c79f0a1fb25e7
Florian Mösch <florian.moesch@draeger.com>
2024-11-14 15:09:28 +0100

    first development release for 38C3

--- pretalx_zammad/forms.py
+++ pretalx_zammad/forms.py
@@ -1,17 +1,33 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+from hierarkey.forms import HierarkeyForm
 
-from i18nfield.forms import I18nModelForm
 
-from .models import ZammadSettings
+class SettingsForm(HierarkeyForm):
+    zammad_url = forms.URLField(
+        label=_("Base URL"),
+        widget=forms.URLInput(attrs={"placeholder": "https://zammad.org/"}),
+        help_text=_("Base URL for Zammad."),
+    )
 
+    zammad_user = forms.CharField(
+        label=_("User"),
+        help_text=_("Username for Zammad API."),
+    )
 
-class ZammadSettingsForm(I18nModelForm):
+    zammad_token = forms.CharField(
+        label=_("Access Token"),
+        widget=forms.PasswordInput(
+            attrs={
+                "placeholder": "XXxxXxxxxxXXXXXXXxXxXxxXxxXx_xXXxXxXxXXXxXXxXXxXXXxXxxXXXXXXxxXx"
+            },
+            render_value=True,
+        ),
+        help_text=_("Access token for Zammad API."),
+    )
 
-    def __init__(self, *args, event=None, **kwargs):
-        self.instance, _ = ZammadSettings.objects.get_or_create(event=event)
-        super().__init__(*args, **kwargs, instance=self.instance, locales=event.locales)
-
-    class Meta:
-        model = ZammadSettings
-        fields = ("some_setting", )
-        widgets = {}
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        event = kwargs.get("obj")
+        if not event.settings.zammad_group:
+            self.fields["zammad_group"].initial = event.slug
