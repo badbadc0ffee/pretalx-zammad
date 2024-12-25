69bf7a426bc75dffa955e1b8beb69920ba391c6d
Florian Moesch <florian@moesch.org>
2024-12-25 15:42:20 +0100

    add signal for samaware

--- pretalx_zammad/signals.py
+++ pretalx_zammad/signals.py
@@ -25,7 +25,7 @@ def pretalx_zammad_settings(sender, request, **kwargs):
 
 
 @receiver(submission_form_html)
-def pretalx_zammad_html_below_submission_form(sender, request, submission, **kwargs):
+def pretalx_zammad_submission_form_html(sender, request, submission, **kwargs):
     if submission is None:
         return None
     try:
@@ -67,3 +67,47 @@ def pretalx_zammad_html_below_submission_form(sender, request, submission, **kwa
     except Exception:
         messages.error(request, "Zammad plugin failure")
     return None
+
+
+try:
+    from samaware.signals import submission_html
+
+    @receiver(submission_html)
+    def samaware_submission_html(sender, request, submission, **kwargs):
+        if submission is None:
+            return None
+        try:
+            api_url = sender.settings.zammad_url + "api/v1/"
+            ticket_url = sender.settings.zammad_url + "#ticket/zoom/"
+            user = sender.settings.zammad_user
+            token = sender.settings.zammad_token
+        except Exception:
+            messages.warning(request, "Zammad plugin configuration is incomplete.")
+            return None
+        try:
+            client = ZammadAPI(url=api_url, username=user, http_token=token)
+            tickets = client.ticket.search(f"tags:{submission.code}")._items
+            if len(tickets) == 0:
+                return None
+            result = "<h3>Zammad</h3>"
+            for ticket in tickets:
+                id = ticket["id"]
+                title = ticket["title"]
+                state = ticket["state"]
+                group = ticket["group"]
+                result += f"""
+                <ul class="list-plain">
+                    <i class="fa fa-circle-o"></i>
+                    <a href='{ticket_url}{id}'>{id}</a> : {title}
+                    <small class="form-text text-muted">{state} in group {group}</small>
+                </ul>
+                """
+            return result
+        except ConnectionError:
+            messages.warning(request, "Zammad plugin connection error.")
+        except Exception:
+            messages.error(request, "Zammad plugin failure")
+        return None
+
+except ImportError:
+    pass
