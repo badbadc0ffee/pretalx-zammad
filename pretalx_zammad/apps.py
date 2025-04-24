01cfa7f059ef8c7ed9a1b2a2aa3f67a4354a250e
Florian Moesch <florian@moesch.org>
2025-04-24 14:54:44 +0200

    minor update to description and translation

--- pretalx_zammad/apps.py
+++ pretalx_zammad/apps.py
@@ -6,20 +6,19 @@ from . import __version__
 
 class PluginApp(AppConfig):
     name = "pretalx_zammad"
-    verbose_name = "pretalx Zammad plugin"
+    verbose_name = "pretalx Zammad"
 
     class PretalxPluginMeta:
-        name = "pretalx Zammad plugin"
-        author = "Florian Moesch"
+        name = "pretalx Zammad"
+        author = "Florian Mösch"
         description = gettext_lazy(
-            """pretalx plugin for Zammad issue tracker.
-
-This plugin allows you to link to tickets in a Zammad issue tracker.
-
-The plugin will match the e-mail adresses of speakers and the six digit speaker and session codes from pretalx with the customer e-mail addresses and tags in Zammad and show the related Zammad ticket title, ticket state and ID on the speaker and session pages in the orga interface.
-
-To manually link Zammad tickets to speakers or sessions in pretalx, you can simply add the six digit code of a speaker or a submission to the tags within Zammad.
-"""
+            "This plugin allows you to link to tickets in a Zammad issue tracker. pretalx Zammad will match the e-mail "
+            "adresses of speakers and the six digit speaker and session codes from pretalx with the customer e-mail "
+            "addresses and tags in Zammad and show the related Zammad ticket title, ticket state and ID on the speaker "
+            "and session pages in the orga interface."
+            "\n"
+            "To manually link Zammad tickets to speakers or sessions in pretalx, you can simply add the six digit code "
+            "of a speaker or a submission to the tags within Zammad."
         )
         visible = True
         version = __version__
